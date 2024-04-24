157c6214487a0fe8b847cbc39524c664a2c0b9a1
Mauro Dattilo <mau.dattilo@gmail.com>
2024-04-24 19:13:09 +0200

    fix: remove svelte5 self closing html tags warnings

--- src/routes/(auth)/auth/login/+page.svelte
+++ src/routes/(auth)/auth/login/+page.svelte
@@ -46,7 +46,7 @@
   </div>
   <div class="relative">
     <div class="absolute inset-0 flex items-center">
-      <span class="w-full border-t" />
+      <span class="w-full border-t"></span>
     </div>
     <div class="relative flex justify-center text-xs uppercase">
       <span class="bg-card px-2 text-muted-foreground"> {m.auth_login_textSeparator()} </span>
